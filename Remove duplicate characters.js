1390f8a18abe7027545ee1615a73d956d60f9598
陈爽-Jack Chen <qingyushijie@126.com>
2017-10-19 19:46:32 +0800

    Remove all duplicate characters from an Array
    This is an example of using Object instance to remove all duplicate characters from an Array instance.

---
+++ Remove duplicate characters.js
@@ -0,0 +1,19 @@
+<script>
+	// Test arry
+	var arry = [1,2,2,3,4,5,5,10];
+	var obj= {};
+
+	// Using object property uniqueness to remove the duplicate characther
+	for (var i=0; i<arry.length; i++) {
+		obj[arry[i]] = true;
+		for(var j=i+1; j<arry.length;j++){
+			if(obj.hasOwnProperty(arry[j])){
+				arry.splice(j,1);
+				j = j-1;
+			}
+		}
+	}
+
+	// print arry in browser console
+	console.log(arry);
+</script>
